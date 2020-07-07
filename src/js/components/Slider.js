642f6e4d22448dfe7baf06e90794bcbb6ac655ed
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2020-07-07 18:40:18 +0200

    Create basic class structure
    What is more I import this class to index.js

---
+++ src/js/components/Slider.js
@@ -0,0 +1,7 @@
+class Slider{
+    constructor(){
+
+    }
+}
+
+export default Slider;
--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,8 @@ import '../scss/main.scss';
 
 // uncomment the lines below to enable PWA
 import {registerSW} from './pwa.js';
+import Slider from './components/Slider.js';
+
 registerSW();
 
 /* place your code below */
