0726878da0e04e14d4e6a1d295bf7a2a2004ff6c
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2020-07-12 22:18:42 +0200

    Add a new method
    This method is responsible for fetching data about the team based on
    which crest the user has clicked. This method is similar to an async
    method from Slider

--- src/js/components/ClubsInfo.js
+++ src/js/components/ClubsInfo.js
@@ -1,8 +1,12 @@
+import {API_TOKEN} from "../config.js";
 class ClubsInfo{
     constructor(clubsInfoWrapper, sliderWrapper) {
         //properties related with DOM
         this.clubsInfoWrapper = clubsInfoWrapper;
         this.sliderWrapper = sliderWrapper;
+
+        //properties related with data
+        this.API = 'https://api.football-data.org/v2/teams/';
 
         //calling methods
         this.crestClickHandling();
@@ -14,6 +18,22 @@ class ClubsInfo{
 
             console.log(target);
         });
+    }
+    async getClubInfo(clubID){
+        const API_KEY = `${this.API}${clubID}`
+         try {
+             const clubInfoRespone = await fetch(API_KEY, {
+                 "method": "GET",
+                 "headers": {
+                     "X-Auth-Token": API_TOKEN,
+                 }
+             })
+             const clubInfo = await clubInfoRespone.json();
+             //it is an object with information about the club
+             return clubInfo;
+         } catch (err) {
+             throw new Error(err);
+         }
     }
 }
 
