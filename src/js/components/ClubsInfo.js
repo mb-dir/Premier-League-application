7f499bf573cfd69889b670df89f4c043b20a64ba
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2020-07-12 18:05:11 +0200

    Add a new method
    This method is responsible for handling click on the crest. When the
    user clicks on one of the crest, the application will send the request to resource(based on data-club-id) and will download the
    information about the club.
    
    Now this method only logs event target

--- src/js/components/ClubsInfo.js
+++ src/js/components/ClubsInfo.js
@@ -2,6 +2,16 @@ class ClubsInfo{
     constructor(clubsInfoWrapper, sliderWrapper) {
         this.clubsInfoWrapper = clubsInfoWrapper;
         this.sliderWrapper = sliderWrapper;
+
+        this.crestClickHandling();
+    }
+
+    crestClickHandling(){
+        this.sliderWrapper.addEventListener('click', (e)=>{
+            const {target} = e;
+
+            console.log(target);
+        });
     }
 }
 
