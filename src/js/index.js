0f4f1f505dd1c4323f0fe76fccbfeea044f86335
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2020-07-12 17:53:46 +0200

    Import the class
    Import and use

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,7 @@ import '../scss/main.scss';
 // uncomment the lines below to enable PWA
 import {registerSW} from './pwa.js';
 import Slider from './components/Slider.js';
+import ClubsInfo from './components/ClubsInfo.js';
 
 registerSW();
 
@@ -18,3 +19,4 @@ const clubsInfoWrapper = document.querySelector('#clubsInfoWrapper');
 
 //components handling
 const clubsSlider = new Slider(sliderWrapper, btnPrev, btnNext);
+const clubsInfo = new ClubsInfo(clubsInfoWrapper, sliderWrapper);
